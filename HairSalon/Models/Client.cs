6b1d119c0d6c061a762fa054fcc9233d6ccbe1ec
Brian Scherner <brianscherner@gmail.com>
2023-10-25 15:42:57 -0700

    Add route and view for Clients list. Fix property type in Client model.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,7 +2,7 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    public string StylistId { get; set; }
+    public int StylistId { get; set; }
     public int ClientId { get; set; }
     public string Name { get; set; }
     public Stylist Stylist { get; set; }
